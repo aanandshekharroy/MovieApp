46f485cdad8d96b542a142693059338d37d5371e
theseus007 <2013001@iiitdmj.ac.in>
2016-06-13 11:17:25 +0530

    Syncadapter working

--- MovieApp/app/src/main/java/com/example/theseus/movieapp/sync/MovieSyncAdapter.java
+++ MovieApp/app/src/main/java/com/example/theseus/movieapp/sync/MovieSyncAdapter.java
@@ -2,15 +2,36 @@ package com.example.theseus.movieapp.sync;
 
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.annotation.TargetApi;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.SyncResult;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
+import com.example.theseus.movieapp.BuildConfig;
 import com.example.theseus.movieapp.R;
+import com.example.theseus.movieapp.data.MovieContract;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Vector;
 
 public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
     public final String LOG_TAG = MovieSyncAdapter.class.getSimpleName();
@@ -18,11 +39,78 @@ public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
     public MovieSyncAdapter(Context context, boolean autoInitialize) {
         super(context, autoInitialize);
     }
+    private String mSortBy;
+    public String getSortBy(){
+        SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(getContext());
+        String sortBy=prefs.getString("sort_by_key","popular");
+        return sortBy;
+    }
 
     @Override
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
-        Log.d(LOG_TAG, "onPerformSync Called.");
+//        Log.d(LOG_TAG, "onPerformSync Called.");
+        String sortBy=getSortBy();
+        Log.d(LOG_TAG,"error 1");
+        this.mSortBy=sortBy;
+        String baseUrl = null;
+        final String APP_KEY="api_key";
+        // params[0]="popular";
+        if(sortBy.contains(MovieContract.FavouritesEntry.TABLE_NAME)){
+            onPostExecute(MovieContract.FavouritesEntry.CONTENT_URI);
+            return;
+        }
+        baseUrl="http://api.themoviedb.org/3/movie/"+sortBy+"?";
 
+        Uri builtUri=Uri.parse(baseUrl).buildUpon()
+                .appendQueryParameter(APP_KEY, BuildConfig.MOVIE_API_KEY).build();
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader=null;
+        String movieData = null;
+        try {
+            URL url=new URL(builtUri.toString());
+            urlConnection=(HttpURLConnection)url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+            InputStream inputStream=urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                onPostExecute(null);
+                return ;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                onPostExecute(null);
+                return ;
+            }
+            movieData = buffer.toString();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(urlConnection!=null){
+                urlConnection.disconnect();
+            }
+            if(reader!=null){
+                try {
+                    reader.close();
+                }catch (final IOException e){
+                    Log.e(LOG_TAG,"Error closing stream");
+                }
+            }
+        }
+        try {
+            onPostExecute(jsonParser(movieData));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        onPostExecute(null);
     }
 
     /**
@@ -36,7 +124,171 @@ public class MovieSyncAdapter extends AbstractThreadedSyncAdapter {
         ContentResolver.requestSync(getSyncAccount(context),
                 context.getString(R.string.content_authority), bundle);
     }
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    protected void onPostExecute(Uri uri){
+        Cursor cursor=null;
+        if(uri!=null){
+            if(uri.toString().contains(MovieContract.FavouritesEntry.TABLE_NAME)){
+//                    cursor=mContext.getContentResolver().query(MovieContract.FavouritesEntry.CONTENT_URI,movieProjections,null,null,null,null);
+//                    mImageAdapter.swapCursor(cursor);
+            }else{
+                try {
+                    cursor = getContext().getContentResolver().query(uri,new String[]{MovieContract.MoviesEntry.COLUMN_MOVIE_ID},null,null,null,null);
+                    if(cursor.moveToFirst()){
+                        do{
+                            fetchTrailersAndReviews(cursor.getString(0));
+                        }while (cursor.moveToNext());
 
+                    }
+                }finally {
+                    if (cursor != null) {
+                        cursor.close();
+                    }
+                }
+            }
+        }
+    }
+    private Uri jsonParser(String movieData) throws JSONException {
+        if(movieData==null){
+            return null;
+        }
+        JSONObject movies=new JSONObject(movieData);
+        JSONArray movieArray=movies.getJSONArray("results");
+        Vector<ContentValues> cvVector=new Vector<>(movieArray.length());
+        for(int i=0;i<movieArray.length();i++){
+            JSONObject movie=movieArray.getJSONObject(i);
+            ContentValues values=new ContentValues();
+            String calue="id="+movie.getString("id")+"\n"+
+                    "title:"+movie.getString("title")+"\n"+
+                    "overview: "+movie.getString("overview")+"\n"
+                    +"votes: "+movie.getString("vote_average")+"\n"+"releasing: "+movie.getString("release_date")+"\n"+
+                    "poster: "+movie.getString("poster_path")+"\n";
+
+            values.put(MovieContract.MoviesEntry.COLUMN_MOVIE_ID,movie.getString("id"));
+            values.put(MovieContract.MoviesEntry.COLUMN_TITLE,movie.getString("title"));
+            values.put(MovieContract.MoviesEntry.COLUMN_SYNOPSIS,movie.getString("overview"));
+            values.put(MovieContract.MoviesEntry.COLUMN_VOTES_AVG,movie.getString("vote_average"));
+            values.put(MovieContract.MoviesEntry.COLUMN_RELEASE_DATE, movie.getString("release_date"));
+            values.put(MovieContract.MoviesEntry.COLUMN_POSTER,movie.getString("poster_path"));
+            values.put(MovieContract.MoviesEntry.COLUMN_SORT_BY,mSortBy);
+            cvVector.add(values);
+        }
+        if(cvVector.size()>0){
+            ContentValues[] contentValuesArray=new ContentValues[cvVector.size()];
+            cvVector.toArray(contentValuesArray);
+            getContext().getContentResolver().bulkInsert(MovieContract.MoviesEntry.CONTENT_URI_GENRE,contentValuesArray);
+        }
+        return MovieContract.MoviesEntry.buildUriFromSortOrder(mSortBy);
+    }
+    private void fetchTrailersAndReviews(String movieId){
+        String baseUrlTrailer = null;
+        String baseUrlReviews = null;
+        final String APP_KEY="api_key";
+//            movieId=params[0];
+        baseUrlTrailer="http://api.themoviedb.org/3/movie/";
+        baseUrlReviews="http://api.themoviedb.org/3/movie/";
+        Uri builtUriTrailer=Uri.parse(baseUrlTrailer).buildUpon()
+                .appendPath(movieId)
+                .appendPath("videos")
+                .appendQueryParameter(APP_KEY,BuildConfig.MOVIE_API_KEY).build();
+        Uri builtUriReviews=Uri.parse(baseUrlTrailer).buildUpon()
+                .appendPath(movieId)
+                .appendPath("reviews")
+                .appendQueryParameter(APP_KEY, BuildConfig.MOVIE_API_KEY).build();
+
+        String trailersData=getJson(builtUriTrailer);
+        String reviewsData=getJson(builtUriReviews);
+        try {
+            trailers(trailersData,movieId);
+            reviews(reviewsData,movieId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return ;
+    }
+
+    private  void reviews(String reviewsData, String movieId) throws JSONException {
+        JSONObject jsonObject=new JSONObject(reviewsData);
+        JSONArray reviewsArray=jsonObject.getJSONArray("results");
+        Vector<ContentValues> cvVarray=new Vector<>(reviewsArray.length());
+        for(int i=0;i<reviewsArray.length();i++){
+            JSONObject Review=reviewsArray.getJSONObject(i);
+            ContentValues values=new ContentValues();
+            values.put(MovieContract.ReviewsEntry.COLUMN_MOVIE_ID,movieId);
+            values.put(MovieContract.ReviewsEntry.COLUMN_AUTHOR,Review.getString("author"));
+            values.put(MovieContract.ReviewsEntry.COLUMN_CONTENT,Review.getString("content"));
+            cvVarray.add(values);
+        }
+        if(cvVarray.size()>0){
+            ContentValues[] contentValues=new ContentValues[cvVarray.size()];
+            cvVarray.toArray(contentValues);
+            getContext().getContentResolver().bulkInsert(MovieContract.ReviewsEntry.CONTENT_URI,contentValues);
+        }
+    }
+
+    private  void trailers(String trailer, String movieId) throws JSONException {
+
+        JSONObject trailerObject=new JSONObject(trailer);
+        JSONArray trailersArray=trailerObject.getJSONArray("results");
+        String baseUrl="https://www.youtube.com/watch?v=";
+        Vector<ContentValues> cvVector=new Vector<>(trailersArray.length());
+        for (int i=0;i<trailersArray.length();i++){
+            JSONObject Trailer=trailersArray.getJSONObject(i);
+            ContentValues values=new ContentValues();
+            values.put(MovieContract.TrailersEntry.COLUMN_MOVIE_ID, movieId);
+            values.put(MovieContract.TrailersEntry.COLUMN_TRAILER_URL, baseUrl + Trailer.getString("key"));
+            cvVector.add(values);
+        }
+        if(cvVector.size()>0){
+            ContentValues[] contentValues=new ContentValues[cvVector.size()];
+            cvVector.toArray(contentValues);
+            getContext().getContentResolver().bulkInsert(MovieContract.TrailersEntry.CONTENT_URI,contentValues);
+        }
+    }
+    private String getJson(Uri builtUri) {
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader=null;
+        String data=null;
+        try {
+            URL url=new URL(builtUri.toString());
+            urlConnection=(HttpURLConnection)url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+            InputStream inputStream=urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0 ){
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+            data = buffer.toString();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if(urlConnection!=null){
+                urlConnection.disconnect();
+            }
+            if(reader!=null){
+                try {
+                    reader.close();
+                }catch (final IOException e){
+                    Log.e(LOG_TAG,"Error closing stream");
+                }
+            }
+        }
+        return data;
+    }
     /**
      * Helper method to get the fake account to be used with SyncAdapter, or make a new one
      * if the fake account doesn't exist yet.  If we make a new account, we call the
