0947c1c7066e5045d459acdf61b00a9cff0092db
theseus007 <2013001@iiitdmj.ac.in>
2016-06-14 21:52:54 +0530

    action bar on details view

--- MovieApp/app/src/main/java/com/example/theseus/movieapp/activity/DetailActivity.java
+++ MovieApp/app/src/main/java/com/example/theseus/movieapp/activity/DetailActivity.java
@@ -1,8 +1,11 @@
 package com.example.theseus.movieapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.theseus.movieapp.R;
 import com.example.theseus.movieapp.fragments.DetailActivityFragment;
@@ -28,5 +31,23 @@ public class DetailActivity extends AppCompatActivity {
                     .commit();
         }
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
